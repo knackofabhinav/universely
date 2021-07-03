87d99490d2dff0c26f5c4b71d94fc4bb80ba27a5
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-07-04 04:07:05 +0530

    create new post

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,13 +1,62 @@
 const { Router } = require("express");
-
+const { Post } = require("../models/post.model");
+const { User } = require("../models/user.model");
+const { Comment } = require("../models/comment.model");
 const router = Router();
 
-router.route("/").get(async (req, res) => {
+router.route("/").post(async (req, res) => {
   try {
-    res.send("post hai nahi abhi");
+    const user = await User.findById(req.body.userId);
+    const post = new Post({
+      author: req.body.userId,
+      caption: req.body.caption,
+    });
+    const savedPost = await post.save();
+    user.posts.push(savedPost._id);
+    await user.save();
+    const populatedUser = await User.findById(req.body.userId).populate({
+      path: "posts",
+    });
+    res.json({ success: true, posts: populatedUser.posts });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, errMessage: e });
+  }
+});
+
+router.route("/likes").post(async (req, res) => {
+  try {
+    const { userId, postId } = req.body;
+    const post = await Post.findById(postId);
+    post.likes.push(userId);
+    await post.save();
+    res.json({ success: true, post });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, errMessage: e });
+  }
+});
+
+router.route("/comments").post(async (req, res) => {
+  const { userId, postId, comment } = req.body;
+  try {
+    const post = await Post.findById(postId);
+    const newComment = new Comment({
+      userId,
+      commentText: comment,
+      post: postId,
+    });
+    await newComment.save();
+    post.comments.push(newComment._id);
+    await post.save();
+    console.log(post);
+    const populatedPost = await Post.findById(postId).populate({
+      path: "comments",
+    });
+    console.log(populatedPost.comments);
+    res.json({ success: true, comments: populatedPost.comments });
   } catch (e) {
     console.log(e);
   }
 });
-
 module.exports = router;
