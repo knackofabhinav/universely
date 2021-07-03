493299c451a5d57fc822195640710353c7b83a20
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-07-04 04:05:30 +0530

    added routes

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const app = express();
 const user = require("./routes/user.route");
 const auth = require("./middleware/auth");
 const post = require("./routes/post.route");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 connectDB();
 
 app.use("/", user);
 app.use("/posts", auth, post);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+//  404 Route Handler
+app.use((req, res) => {
+  res.status(404).json({ success: false });
+});
+
+/**
+ * Error Handler
+ * Don't move
+ */
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "error occured, see the errMessage key for more details",
+    errorMessage: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express App is Listening at http://localhost:${PORT}`);
+});
