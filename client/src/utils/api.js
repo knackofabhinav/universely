eeab7f4f5c0413715135227986793d1b7ec2a6ca
Abhinav Niranjan <66178038+knackofabhinav@users.noreply.github.com>
2021-10-11 02:08:17 +0530

    fixed api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const setInitialAPI = () => {
-  axios.defaults.baseURL = "http://localhost:3000/";
+  axios.defaults.baseURL = "https://universely.herokuapp.com/";
   axios.defaults.headers.common["Authorization"] = JSON.parse(
     localStorage.getItem("authToken")
   );
