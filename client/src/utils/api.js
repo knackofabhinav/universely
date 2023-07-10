87eccc4d1718a9e4b2670172d9f714cf28cfc33a
Abhinav Patel <knackofabhinav@gmail.com>
2023-07-10 19:40:25 +0530

    fix: added the new endpoint

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const setInitialAPI = () => {
-  axios.defaults.baseURL = "https://universely.herokuapp.com/";
+  axios.defaults.baseURL = "http://167.71.233.233/";
   axios.defaults.headers.common["Authorization"] = JSON.parse(
     localStorage.getItem("authToken")
   );
